f1633b2e81caacf42079aa97cd35c3c4e121fd0a
cheeter_Lee <johnson991210@gmail.com>
2023-01-04 21:27:18 +0800

    trying to deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -94,7 +94,7 @@ async function main() {
 	)
 
 	await new Promise<void>((resolve) =>
-		httpServer.listen({ port: process.env.PORT }, resolve)
+		httpServer.listen({ port: process.env.PORT || 4000 }, resolve)
 	)
 
 	console.log(`🚀 Server ready at http://localhost:${process.env.PORT}/graphql`)
