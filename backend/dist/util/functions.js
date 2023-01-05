ac05de0862fb782c29274a6d807c9b9e653e76cc
cheeter_Lee <johnson991210@gmail.com>
2023-01-05 22:37:35 +0800

    trying to delploy zzz

--- backend/dist/util/functions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userIsConversationParticipant = void 0;
-function userIsConversationParticipant(participants, userId) {
-    return !!participants.find((participant) => participant.userId === userId);
-}
-exports.userIsConversationParticipant = userIsConversationParticipant;
